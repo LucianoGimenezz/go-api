da9597ced7cc583e5aff48f379ba455c9d061261
Luciano Gimenez <luciano.gimenezz21@gmail.com>
2024-10-06 21:08:27 -0300

    add: function to retrieve data from body

--- handler_image.go
+++ handler_image.go
@@ -1,15 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
-	// "path/filepath"
 )
 
+type Data struct {
+	FileName string `json:"fileName"`
+}
+
 func HandlerImage(w http.ResponseWriter, r *http.Request) {
+
+	// Extract data from the body
+
+	contentType := strings.Split(r.Header.Get("Content-Type"), ";")[0]
+
+	var fileName string = ""
+
+	switch contentType {
+	case "application/json":
+		var data Data
+
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			ResponseWithError(w, 500, err.Error())
+			return
+		}
+
+		fileName = data.FileName
+	case "multipart/form-data":
+
+		err := r.ParseMultipartForm(10 << 20)
+
+		if err != nil {
+			ResponseWithError(w, 500, err.Error())
+		}
+
+		fileName = r.FormValue("fileName")
+	}
+
 	file, header, err := r.FormFile("file")
 
 	if err != nil {
@@ -18,8 +52,14 @@ func HandlerImage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	defer file.Close()
-	// ext := filepath.Ext(header.Filename)
-	fileName := strings.ReplaceAll(header.Filename, " ", "_")
+	ext := filepath.Ext(header.Filename)
+
+	if fileName == "" {
+		fileName = strings.ReplaceAll(header.Filename, " ", "_")
+	} else {
+		fileName = fmt.Sprintf("%s%s", fileName, ext)
+	}
+
 	outFile, err := os.Create(fileName)
 
 	if err != nil {
